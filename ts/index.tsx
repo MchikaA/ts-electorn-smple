972984ec7cac1d12932a1597c5edaaeb970df26a
masachika.kuwabara <kuwabara@mla.osp.co.jp>
2019-04-19 14:13:39 +0900

    新しいモジュールをimport

--- ts/index.tsx
+++ ts/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux'; // 追加
+import UserForm from './components/UserForm'; // 追加
+import Store from './Store'; // 追加
 
 const container = document.getElementById('contents');
 
